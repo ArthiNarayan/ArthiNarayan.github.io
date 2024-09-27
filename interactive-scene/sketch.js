cf9184696a51ecd3eb2083b902963e16789c5571
ArthiNarayan <arthi.narayan333@gmail.com>
2024-09-27 00:09:11 -0600

    did some of start screen

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,26 +1,38 @@
 // Interactive Scene
 // PUT HEADER
 
-let start = true;
+let state = "start";
+let circlecat; 
+
+function preload() {
+  circlecat = loadImage("circlecat.png");
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
   swapState();
+  image(circlecat, 20, 20, circlecat.width * 0.25, circlecat.height * 0.25);
 }
 
 function swapState() {
-  if (start === true) {
-    background("5BB450");
+  if (state === "start") {
+    startScreen();
+  }
+  else if (state === "animal cell") {
+    animalCell();
+  }
+  else if (state === "plant cell") {
+    plantCell();
   }
 }
 
-// function startScreen
-// if (start) {
-//   // all code
-// }
-
-// else {
-
-// }
+function startScreen() {
+  background("#f7e7ce");
+  textSize(50);
+  text('Meowcrobiology', 450, 100);
+}
+// if (screen === 1 && mouseX > 50 && mouseX < 350 && mouseY > 50 && mouseY < 350) {
+//   screen = 2; // Change screen
