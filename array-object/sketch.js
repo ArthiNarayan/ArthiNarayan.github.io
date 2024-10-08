0d49edad66e6b5e3a4149ddeec05613d8e9478fd
ArthiNarayan <arthi.narayan333@gmail.com>
2024-10-08 14:16:01 -0600

    started making variables for a function to draw mandalas

---
+++ array-object/sketch.js
@@ -0,0 +1,25 @@
+// Arrays and Object Notation Assignment
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+
+}
+
+function mandala_pattern {
+  let time = 0
+  let deltaTime = 0.001
+}
+
+function spawnCircle() {
+  
+}
