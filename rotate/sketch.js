6177af4b3c3ae58dd74664cf6f8dbdcc8bec5cd5
ArthiNarayan <arthi.narayan333@gmail.com>
2024-10-17 14:16:09 -0600

    added color to mandala function

---
+++ rotate/sketch.js
@@ -0,0 +1,26 @@
+// Translate and Rotate
+
+
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  rectMode(CENTER);
+}
+
+function draw() {
+  background(220);
+
+  push();
+  translate(200, 200);
+  rotate(mouseX);
+  fill("pink");
+  square(0, 0, 100);
+  pop(); //reset to the push version
+
+  fill("pink");
+  rect(width/2, height - 200, width * 2, 400)
+}
+
+
