d13963398fee3d6e0734ec93dbefecf11664c66b
ArthiNarayan <arthi.narayan333@gmail.com>
2024-10-29 14:15:04 -0600

    initialized some variables for the mancala board

---
+++ grid-based-game/sketch.js
@@ -0,0 +1,18 @@
+// Grid-Based Game Assignment
+// Arthi Narayan
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let mancala_board;
+const BOARD_SIZE = 7;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
